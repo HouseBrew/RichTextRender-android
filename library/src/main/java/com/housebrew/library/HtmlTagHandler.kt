8a8a4671946c5d4bc752146f9d33b471909e9af5
jason-lhy <hyliu921@gmail.com>
2018-07-28 16:05:38 +0800

    add support to nested tag styling

--- library/src/main/java/com/housebrew/library/HtmlTagHandler.kt
+++ library/src/main/java/com/housebrew/library/HtmlTagHandler.kt
@@ -8,14 +8,16 @@ import android.text.style.StyleSpan
 import org.htmlcleaner.ContentNode
 import org.htmlcleaner.HtmlCleaner
 import org.htmlcleaner.TagNode
+import java.util.Stack
 
 object HtmlTagHandler {
 
-    private const val htmlString: String = "<p>Atlanta's <strong>leaders <i>faced</i></strong> an urgent decision that would impact every citizen of their city. An \"extreme weather\" event was bearing down on them, and they needed a plan of action.</p>"
+    private const val htmlString: String = "<p>Atlanta's <strike><strong>leaders <br><i>faced</i></strong></strike> an urgent decision that would impact every citizen of their city. An \"extreme weather\" event was bearing down on them, and they needed a plan of action.</p>"
 
     private val cleaner by lazy { HtmlCleaner() }
 
     private val SUPPORTED_TAG = setOf(
+        "html", "body",
         "p", "ul", "li", "ol", "sup", "sub", "b", "i",
         "strike", "strong", "em", "br", "h1", "h2", "h3",
         "h4", "h5", "h6", "big", "small", "a", "font"
@@ -40,23 +42,37 @@ object HtmlTagHandler {
         return spannableStrBuilder
     }
 
-    private fun loopInnerTags(builder: SpannableStringBuilder, node: TagNode) {
-        println(node.name)
+    private fun loopInnerTags(builder: SpannableStringBuilder, node: TagNode, attrStack: Stack<String> = Stack()) {
+        // Exception case, no span needed
+        when (node.name) {
+            !in SUPPORTED_TAG -> return
+            "br" -> {
+                builder.append("\n")
+                return
+            }
+        }
+
+        attrStack.push(node.name)
         for (childNode in node.allChildren) {
             when (childNode) {
                 is ContentNode -> {
                     val startIndex = builder.length
                     builder.append(childNode.content)
                     val endIndex = builder.length
-
-                    when (node.name) {
-                        "b", "strong" -> builder.setSpan(StyleSpan(Typeface.BOLD), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                        "i" -> builder.setSpan(StyleSpan(Typeface.ITALIC), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-                        else -> builder.setSpan(StrikethroughSpan(), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                    for (attr in attrStack) {
+                        when (attr) {
+                            "b", "strong" -> builder.setSpan(StyleSpan(Typeface.BOLD), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                            "i" -> builder.setSpan(StyleSpan(Typeface.ITALIC), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                            "strike" -> builder.setSpan(StrikethroughSpan(), startIndex, endIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                            "br" -> builder.append("\n")
+                        }
                     }
                 }
-                is TagNode -> loopInnerTags(builder, childNode)
+                is TagNode -> {
+                    loopInnerTags(builder, childNode, attrStack)
+                }
             }
         }
+        attrStack.pop()
     }
 }
